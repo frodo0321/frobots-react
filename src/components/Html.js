da68baa50be57b0986bef6d2872e6d69ad49a2b0
Tyler Haun <frodo03211@gmail.com>
2018-05-31 23:01:27 -0700

    updated base html with better support for browsers + mobile

--- src/components/Html.js
+++ src/components/Html.js
@@ -1,7 +1,17 @@
 const Html = ({ body, title }) => `
     <!DOCTYPE html>
-    <html>
+    <!--[if IE 7]>
+    <html class="ie ie7" lang="en-US">
+    <![endif]-->
+    <!--[if IE 8]>
+    <html class="ie ie8" lang="en-US">
+    <![endif]-->
+    <!--[if !(IE 7) & !(IE 8)]><!-->
+    <html lang="en-US">
+    <!--<![endif]-->
         <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width">
         <title>${title}</title>
         <link rel="stylesheet" type="text/css" href="/style.css">
         </head>
